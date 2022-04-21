0322495822d3e165968df100903109343f0b8e81
GenerateMe <generateme.blog@gmail.com>
2022-04-21 12:37:33 +0200

    group name fix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.sciclog/clojisr-rengine "0.1.0"
+(defproject org.scicloj/clojisr-rengine "0.1.0"
   :description "A wrapper to the newest REngine"
   :url "https://github.com/scicloj/clojisr-rengine"
   :java-source-paths ["java"]
