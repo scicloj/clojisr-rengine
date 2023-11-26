31c85a181e70ba142c73a79b33b6f6dd76448a43
GenerateMe <generateme.blog@gmail.com>
2023-11-26 20:12:56 +0100

    REngine updated

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject org.scicloj/clojisr-rengine "0.1.0"
+(defproject org.scicloj/clojisr-rengine "0.1.2"
   :description "A wrapper to the newest REngine"
   :url "https://github.com/scicloj/clojisr-rengine"
   :java-source-paths ["java"]
-  :javac-options ["-source" "1.8" "-target" "1.8"])
+  :javac-options ["--release" "8"])
